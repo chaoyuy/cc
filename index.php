cc36f39a9833d68be98812df1e3b08050dcb38c7
Chaoyu YANG <https://github.com/chaoyuy/cc.git>
2020-01-15 19:09:35 +1100

    example

--- index.php
+++ index.php
@@ -2,17 +2,22 @@
 require('vendor/autoload.php');
 use oldspice\Navigation;
 use oldspice\Product;
+use oldspice\Category;
 $navigation =Navigation::getNavigation();
 $p = new Product();
 $products = $p -> getProducts();
 
-//Twig
+$cat = new Category();
+$categories = $cat ->getCategories();
+
+//Twigor
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment( $loader );
 //load the template
 $template = $twig -> load( 'home.twig' );
 //output the template to page
 echo $template -> render([
+  'categories' => $categories,
   'navigation' =>$navigation,
   'products' => $products,
   'title' => 'Home Page'
---
+++ src/Category.php
@@ -0,0 +1,54 @@
+<?php
+namespace oldspice;
+
+use oldspice\Database;
+use \Exception;
+class Category extends Database {
+    public $categories  = array();
+    public function __construct() {
+        parent::__construct();
+
+    }
+    public function getCategories() {
+        $query ="SELECT  category_id, category_name 
+         FROM  category 
+         WHERE active=1 
+         ORDER BY category_id ASC";
+
+         try {
+             $statement= $this -> connection -> prepare($query); 
+             if( $statement == false) {
+                 throw new Exception('query error');
+             
+         }
+        }
+         catch( Exception $exc ) {
+             error_log( $exc -> getMessage() );
+         }
+         try {
+             if( $statement -> execute() == false ) {
+                throw new Exception('query error');
+
+             }
+         }
+         catch(Exception $exc) {
+             error_log( $exc -> getMessage() );
+         }
+         $result = $statement -> get_result();
+         //get the result of the query
+         $category_items = array();
+         while($row = $result -> fetch_assoc() ) {
+             array_push( $category_items , $row);
+         }
+         $active =$this -> getActiveCategory();
+         $this -> categories['items'] = $category_items;
+         $this -> categories['active'] = $active;
+     //return  the categories array
+    return  $this-> categories;
+    }
+
+    private function getActiveCategory() {
+        return  ( isset($_GET['category_id'] )  ) ? $_GET['category_id'] : null ;
+    }
+}
+?>
